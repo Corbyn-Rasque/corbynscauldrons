4463302a016bdb325d57e18b77f4432b8d97b6a0
Corbyn-Rasque <corbyn@rasque.dev>
2024-09-30 17:43:55 -0700

    Version 1 - Adding print for when catalog posts

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,6 +14,7 @@ def get_catalog():
         num_green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
 
     if num_green_potions >= 1:
+        print("Posted Catalog")
         return [
                 {
                     "sku": "GREEN_POTION_0",
@@ -24,4 +25,5 @@ def get_catalog():
                 }
             ]
     else:
+        print("Did NOT Post Catalog")
         return []
