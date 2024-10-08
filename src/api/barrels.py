3f8d7de7bf75a07c82aefe26a01e528b4edb4c4c
Corbyn-Rasque <corbyn@rasque.dev>
2024-10-08 00:09:05 -0700

    Version 2 - Alpha (Barrels)

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -3,6 +3,18 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
+from enum import Enum
+from math import sqrt, pow
+from pulp import LpMinimize, LpMaximize, LpProblem, LpStatus, lpSum, LpVariable, value
+
+class BarrelType(Enum):
+    RED     =   (1, 0, 0, 0)
+    GREEN   =   (0, 1, 0, 0)
+    BLUE    =   (0, 0, 1, 0)
+    DARK    =   (0, 0, 0, 1)
+
+    def __str__(self):
+        return str(list(self.value))
 
 router = APIRouter(
     prefix="/barrels",
@@ -42,35 +54,115 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """
     This function will send your purchase order to the barrel seller.
     """
-    print(wholesale_catalog)
+    # print(wholesale_catalog)
+
+    target_potions = [(100, 0, 0, 0), (0, 100, 0, 0), (0, 0, 100, 0), (0, 0, 0, 100)]
+    target_ratio = [0.2, 0.5, 0.2, 0.1]
+    deviation = 15
 
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+        gold, vol_capacity, red, green, blue, dark = connection.execute(sqlalchemy.text(f"""SELECT gold, vol_capacity, red, green, blue, dark
+                                                                                            FROM global_inventory""")).first()
+    
+    current_volumes = [red, green, blue, dark]
+    potion_colors = ['RED', 'GREEN', 'BLUE', 'DARK']
+    sizes = ['MINI', 'SMALL', 'MEDIUM', 'LARGE']
 
-        potions, _, gold = result.first()
+    data = {}
+    for color in potion_colors:
+        data.update({color: {}})
+        for size in sizes:
+            data[color].update({size: {'volume': 0, 'price': 0, 'qty': 0}})
 
-        to_order = "SMALL_GREEN_BARREL"
+    for barrel in wholesale_catalog:
+        color = BarrelType(tuple(barrel.potion_type)).name
+        size = barrel.sku[:barrel.sku.index('_')]
+        data[color][size]['volume'] = barrel.ml_per_barrel
+        data[color][size]['price'] = barrel.price
+        data[color][size]['qty'] = barrel.quantity
 
-        for barrel in wholesale_catalog:
-            if barrel.sku == to_order and gold // barrel.price > 0:
-                return [
-                    {
-                        "sku": f"{to_order}",
-                        "quantity": (gold // barrel.price)
-                    }
-                ]
-        return []
+    targets = {}
+    for color, target_volume in zip(potion_colors, [int(ratio * vol_capacity) for ratio in target_ratio]):
+        targets.update({color: target_volume})
 
-# my_catalog = [Barrel(sku='MEDIUM_RED_BARREL', ml_per_barrel=2500, potion_type=[1, 0, 0, 0], price=250, quantity=10),
-# Barrel(sku='SMALL_RED_BARREL', ml_per_barrel=500, potion_type=[1, 0, 0, 0], price=100, quantity=10),
-# Barrel(sku='MEDIUM_GREEN_BARREL', ml_per_barrel=2500, potion_type=[0, 1, 0, 0], price=250, quantity=10),
-# Barrel(sku='SMALL_GREEN_BARREL', ml_per_barrel=500, potion_type=[0, 1, 0, 0], price=100, quantity=1),
-# Barrel(sku='MEDIUM_BLUE_BARREL', ml_per_barrel=2500, potion_type=[0, 0, 1, 0], price=300, quantity=10),
-# Barrel(sku='SMALL_BLUE_BARREL', ml_per_barrel=500, potion_type=[0, 0, 1, 0], price=120, quantity=10),
-# Barrel(sku='MINI_RED_BARREL', ml_per_barrel=200, potion_type=[1, 0, 0, 0], price=60, quantity=1),
-# Barrel(sku='MINI_GREEN_BARREL', ml_per_barrel=200, potion_type=[0, 1, 0, 0], price=60, quantity=1),
-# Barrel(sku='MINI_BLUE_BARREL', ml_per_barrel=200, potion_type=[0, 0, 1, 0], price=60, quantity=1)]
+    print(targets)
 
-# get_wholesale_purchase_plan(wholesale_catalog = my_catalog)
+    starting_volumes = {}
+    for color, volume in zip(potion_colors, current_volumes):
+        starting_volumes.update({color: volume})
 
-# post_deliver_barrels([Barrel(sku='SMALL_GREEN_BARREL', ml_per_barrel=500, potion_type=[0, 1, 0, 0], price=100, quantity=1)], order_id=7)
+    total_starting_volume = sum(starting_volumes[color] for color in potion_colors)
+    remaining_space = vol_capacity - total_starting_volume
+
+    volume_required = {}
+    for potion in potion_colors:
+        required = targets[potion] - starting_volumes[potion]
+        volume_required[potion] = required if required > 0 else 0
+
+    # DECIDE ON FUNCTIONALITY
+    # model = LpProblem(name = 'Barrel_Optimization', sense = LpMinimize)
+    model = LpProblem(name = 'Barrel_Optimization', sense = LpMaximize)
+
+    # Variables: number of each catalog listing to buy.
+    variables = LpVariable.dicts("Buy", [(potion, size) for potion in potion_colors for size in sizes], lowBound = 0, cat = 'Integer')
+    
+    # Objective Function: DECIDE ON FUNCTIONALITY
+    # model += lpSum(data[potion][size]['price'] * variables[(potion, size)] for potion in potion_colors for size in sizes)
+    model += lpSum(data[potion][size]['volume'] * variables[(potion, size)] for potion in potion_colors for size in sizes)
+
+    # Volume Constraint: functionality may be broken CHECK!
+    model += lpSum(data[potion][size]['volume'] * variables[(potion, size)] for potion in potion_colors for size in sizes) >= volume_required[potion]
+
+    # Inventory Constraint
+    model += lpSum(data[potion][size]['volume'] * variables[(potion, size)] for potion in potion_colors for size in sizes) <= remaining_space
+
+    # Budget Constraint
+    model += lpSum(data[potion][size]['price'] * variables[(potion, size)] for potion in potion_colors for size in sizes) <= gold
+
+    # Availability Constraint
+    for potion in potion_colors:
+        for size in sizes:
+            model += variables[(potion, size)] <= data[potion][size]['qty']  
+    
+    model.solve()
+
+    purchase_plan = []
+    if LpStatus[model.status] == 'Optimal':
+        for color in potion_colors:
+            for size in sizes:
+                if variables[(color, size)].varValue != 0:
+                    purchase_plan.append(
+                        {
+                        "sku": '_'.join([size, color, 'BARREL']),
+                        "ml_per_barrel": data[color][size]['volume'],
+                        "potion_type": BarrelType[color].value,
+                        "price": data[color][size]['price'],
+                        "quantity": variables[(color, size)].varValue
+                        })
+        return purchase_plan
+    else:
+        return purchase_plan
+
+
+if __name__ == "__main__":
+    my_catalog = [Barrel(sku='MEDIUM_RED_BARREL', ml_per_barrel=2500, potion_type=[1, 0, 0, 0], price=250, quantity=10),
+    Barrel(sku='SMALL_RED_BARREL', ml_per_barrel=500, potion_type=[1, 0, 0, 0], price=100, quantity=10),
+    Barrel(sku='MEDIUM_GREEN_BARREL', ml_per_barrel=2500, potion_type=[0, 1, 0, 0], price=250, quantity=10),
+    Barrel(sku='SMALL_GREEN_BARREL', ml_per_barrel=500, potion_type=[0, 1, 0, 0], price=100, quantity=1),
+    Barrel(sku='MEDIUM_BLUE_BARREL', ml_per_barrel=2500, potion_type=[0, 0, 1, 0], price=300, quantity=10),
+    Barrel(sku='SMALL_BLUE_BARREL', ml_per_barrel=500, potion_type=[0, 0, 1, 0], price=120, quantity=10),
+    Barrel(sku='MINI_RED_BARREL', ml_per_barrel=200, potion_type=[1, 0, 0, 0], price=60, quantity=1),
+    Barrel(sku='MINI_GREEN_BARREL', ml_per_barrel=200, potion_type=[0, 1, 0, 0], price=60, quantity=1),
+    Barrel(sku='MINI_BLUE_BARREL', ml_per_barrel=200, potion_type=[0, 0, 1, 0], price=60, quantity=1)]
+
+    print(get_wholesale_purchase_plan(my_catalog))
+
+    # def projection(a, b):
+    #     dot_product_scalar = (sum(i*j for i, j in zip(a, b)) / abs(sum(i**2 for i in b)))
+    #     return [i*dot_product_scalar for i in b]
+
+    # wholesale_catalog_dict = dict.fromkeys()
+
+    # print(BarrelType._member_names_[0] == 'RED')
+    # print(BarrelType['RED'])
+    # print(BarrelType([1, 0, 0, 0]).name)
