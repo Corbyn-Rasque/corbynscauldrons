81c7fd15620863971b343a939537ae09b65644f1
Corbyn-Rasque <corbyn@rasque.dev>
2024-10-09 13:37:36 -0700

    Version 2 - Temp Database Shop Time & Reviews Fix

--- src/database.py
+++ src/database.py
@@ -11,20 +11,22 @@ def database_connection_url():
 
 engine = create_engine(database_connection_url(), pool_pre_ping = True)
 
-# Gets shop time & ratings from Potion Shop
-def shop_info():
-    dotenv.load_dotenv()
 
-    api_key = os.environ.get("SUPABASE_API_KEY")
-    base_url = os.environ.get("SUPABASE_URL")
+if __name__ == '__main__':
+    # Gets shop time & ratings from Potion Shop
+    def shop_info():
+        dotenv.load_dotenv()
 
-    headers = {
-        "apikey": api_key,
-        "Authorization": f"Bearer {api_key}",
-        "Accept": "application/json"
-    }
+        api_key = os.environ.get("SUPABASE_API_KEY")
+        base_url = os.environ.get("SUPABASE_URL")
 
-    return {'api_key': api_key, 'base_url': base_url, 'headers': headers}
+        headers = {
+            "apikey": api_key,
+            "Authorization": f"Bearer {api_key}",
+            "Accept": "application/json"
+        }
 
-time = requests.get(shop_info()['base_url']+'/rest/v1/current_game_time?select=*', headers=shop_info()['headers']).json()
-ratings = requests.get(shop_info()['base_url']+'/rest/v1/rpc/shop_star_ratings', headers=shop_info()['headers']).json()
+        return {'api_key': api_key, 'base_url': base_url, 'headers': headers}
+
+    time = requests.get(shop_info()['base_url']+'/rest/v1/current_game_time?select=*', headers=shop_info()['headers']).json()
+    ratings = requests.get(shop_info()['base_url']+'/rest/v1/rpc/shop_star_ratings', headers=shop_info()['headers']).json()
