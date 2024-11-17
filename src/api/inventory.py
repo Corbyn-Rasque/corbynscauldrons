011bc50aababdffcad0470a35db256746ce34227
Corbyn-Rasque <corbyn@rasque.dev>
2024-11-17 04:16:07 -0800

    Version 4 & 5 - Added capacity purchasing logic.

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -1,19 +1,10 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
-import math
 from sqlalchemy import text
 from src import database as db
-from enum import Enum
+import time
 
-class BarrelType(Enum):
-    RED     =   [1, 0, 0, 0]
-    GREEN   =   [0, 1, 0, 0]
-    BLUE    =   [0, 0, 1, 0]
-    DARK    =   [0, 0, 0, 1]
-
-    def __str__(self):
-        return str(list(self.value))
 
 router = APIRouter(
     prefix="/inventory",
@@ -35,6 +26,11 @@ def get_inventory():
     return dict(inventory)
 
 
+class CapacityPurchase(BaseModel):
+    potion_capacity: int
+    ml_capacity: int
+
+
 # Gets called once a day
 @router.post("/plan")
 def get_capacity_plan():
@@ -42,16 +38,78 @@ def get_capacity_plan():
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     '''
+    
+    capacity_plan = CapacityPurchase(potion_capacity = 0, ml_capacity = 0)
 
-    return {
-        "potion_capacity": 0,
-        "ml_capacity": 0
-        }
+    get_gold = text('''SELECT gold
+                       FROM global''')
 
+    get_potions_pressure =    text('''WITH reset AS (SELECT timestamp AS time
+                                                       FROM resets
+                                                       ORDER BY timestamp DESC
+                                                       LIMIT 1),
+                                             hourly_potion AS (SELECT date_trunc('hour', potion_ledger.timestamp) AS hour,
+                                                                   SUM(potion_ledger.qty)::INTEGER AS quantity_sum
+                                                               FROM potion_ledger, reset
+                                                               WHERE potion_ledger.timestamp >= reset.time
+                                                                   AND potion_ledger.timestamp >= CURRENT_TIMESTAMP - INTERVAL '24 hours'
+                                                               GROUP BY hour),
+                                             hourly_capacity AS (SELECT date_trunc('hour', capacity_ledger.timestamp) AS hour,
+                                                                     SUM(capacity_ledger.potion)::INTEGER AS capacity_sum
+                                                                 FROM capacity_ledger, reset
+                                                                 WHERE capacity_ledger.timestamp >= reset.time
+                                                                 GROUP BY hour)
+                            SELECT SUM(quantity_sum) OVER (ORDER BY hourly_potion.hour ASC) AS num_potions,
+                                   hourly_capacity2.capacity_sum AS capacity
+                            FROM hourly_potion
+                            LEFT JOIN LATERAL (SELECT capacity_sum
+                                               FROM hourly_capacity
+                                               WHERE hourly_capacity.hour <= hourly_potion.hour
+                                               ORDER BY hourly_capacity.hour DESC
+                                               LIMIT 1) AS hourly_capacity2 ON TRUE
+                            ORDER BY hourly_potion.hour ASC''')
+    
+    get_volume_pressure =   text('''WITH reset AS (SELECT timestamp AS time
+                                                   FROM resets
+                                                   ORDER BY timestamp DESC
+                                                   LIMIT 1),
+                                         hourly_barrels AS (SELECT date_trunc('hour', barrel_ledger.timestamp) AS hour,
+                                                                   SUM(barrel_ledger.red
+                                                                     + barrel_ledger.green
+                                                                     + barrel_ledger.blue
+                                                                     + barrel_ledger.dark)::INTEGER AS quantity_sum
+                                                            FROM barrel_ledger, reset
+                                                            WHERE barrel_ledger.timestamp >= reset.time
+                                                                AND barrel_ledger.timestamp >= CURRENT_TIMESTAMP - INTERVAL '24 hours'
+                                                            GROUP BY hour),
+                                         hourly_capacity AS (SELECT date_trunc('hour', capacity_ledger.timestamp) AS hour,
+                                                                    SUM(capacity_ledger.volume)::INTEGER AS volume_sum
+                                                             FROM capacity_ledger, reset
+                                                             WHERE capacity_ledger.timestamp >= reset.time
+                                                             GROUP BY hour)
+                                    SELECT SUM(quantity_sum) OVER (ORDER BY hourly_barrels.hour ASC) AS volume, hourly_capacity2.volume_sum AS capacity
+                                    FROM hourly_barrels
+                                    LEFT JOIN LATERAL (SELECT volume_sum
+                                                       FROM hourly_capacity
+                                                       WHERE hourly_capacity.hour <= hourly_barrels.hour
+                                                       ORDER BY hourly_capacity.hour DESC
+                                                       LIMIT 1) AS hourly_capacity2 ON TRUE
+                                    ORDER BY hourly_barrels.hour ASC''')
+    
+    with db.engine.begin() as connection:
+        
+        gold = connection.execute(get_gold).one()
+        potion_pressure = connection.execute(get_potions_pressure).all()
+        volume_pressure = connection.execute(get_volume_pressure).all()
+    
+    if potion_pressure and min([(inventory - potions) for (potions, inventory) in potion_pressure]) < 5:
+        capacity_plan.potion_capacity = 1 if gold > 2000 else 0
+        gold -= 1000
 
-class CapacityPurchase(BaseModel):
-    potion_capacity: int
-    ml_capacity: int
+    if volume_pressure and min([(inventory - volume) for (volume, inventory) in volume_pressure]) < 500:
+        capacity_plan.potion_capacity = 1 if gold > 2000 else 0
+
+    return dict(capacity_plan)
 
 
 # Gets called once a day
